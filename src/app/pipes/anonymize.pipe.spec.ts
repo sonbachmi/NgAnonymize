f6fa50eee89e4a5ce784cb4acba9dd06ad54a02e
Son Nguyen <son.nguyen@qudy.com.sg>
2020-03-11 20:55:25 +0700

    add unit tests

--- src/app/pipes/anonymize.pipe.spec.ts
+++ src/app/pipes/anonymize.pipe.spec.ts
@@ -1,8 +1,21 @@
-import { AnonymizePipe } from './anonymize.pipe';
+import {AnonymizePipe} from './anonymize.pipe';
+import {AnonymizeMethod} from "../models/anonymize.models";
 
 describe('AnonymizePipe', () => {
   it('create an instance', () => {
     const pipe = new AnonymizePipe();
     expect(pipe).toBeTruthy();
   });
+  it('produce output same length as input', () => {
+    const pipe = new AnonymizePipe();
+    const input = 'abcdef';
+    expect(pipe.transform(input).length).toEqual(input.length);
+  });
+  it('shuffle preserves character set', () => {
+    const pipe = new AnonymizePipe();
+    const input = 'abcdef';
+    expect(Array.prototype.every.call(
+      pipe.transform(input, AnonymizeMethod.Shuffle), o => input.indexOf(o) >= 0)).toBeTruthy();
+  });
 });
+
